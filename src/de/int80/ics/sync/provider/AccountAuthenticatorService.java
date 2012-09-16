2150ae1cb41e3887b627016c14ebe145fcbfff68
Jens Kehne <jens@int80.de>
2012-09-16 10:11:58 +0100

    AccountAuthenticator

---
+++ src/de/int80/ics/sync/provider/AccountAuthenticatorService.java
@@ -0,0 +1,104 @@
+package de.int80.ics.sync.provider;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.AccountManager;
+import android.accounts.NetworkErrorException;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+
+public class AccountAuthenticatorService extends Service {
+
+	private static final String TAG = "AccountAuthenticatorService";
+	private static AccountAuthenticatorImpl sAccountAuthenticator = null;
+	
+	private static class AccountAuthenticatorImpl extends
+			AbstractAccountAuthenticator {
+
+		private Context mContext;
+		
+		public AccountAuthenticatorImpl(Context context) {
+			super(context);
+			mContext = context;
+		}
+
+		@Override
+		public Bundle addAccount(AccountAuthenticatorResponse response,
+				String accountType, String authTokenType,
+				String[] requiredFeatures, Bundle options)
+				throws NetworkErrorException {
+			Bundle reply = new Bundle();
+			
+			Intent i = new Intent(mContext, LoginScreen.class);
+			i.setAction("fm.last.android.sync.LOGIN");
+			i.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, response);
+			reply.putParcelable(AccountManager.KEY_INTENT, i);
+
+			return reply;
+		}
+
+		@Override
+		public Bundle confirmCredentials(AccountAuthenticatorResponse response,
+				Account account, Bundle options) throws NetworkErrorException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Bundle editProperties(AccountAuthenticatorResponse response,
+				String accountType) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Bundle getAuthToken(AccountAuthenticatorResponse response,
+				Account account, String authTokenType, Bundle options)
+				throws NetworkErrorException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getAuthTokenLabel(String authTokenType) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Bundle hasFeatures(AccountAuthenticatorResponse response,
+				Account account, String[] features)
+				throws NetworkErrorException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public Bundle updateCredentials(AccountAuthenticatorResponse response,
+				Account account, String authTokenType, Bundle options)
+				throws NetworkErrorException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		IBinder ret = null;
+		if (intent.getAction().equals(android.accounts.AccountManager.ACTION_AUTHENTICATOR_INTENT))
+			ret = getAuthenticator().getIBinder();
+		return ret;
+	}
+
+	private AccountAuthenticatorImpl getAuthenticator() {
+		if (sAccountAuthenticator == null)
+			sAccountAuthenticator = new AccountAuthenticatorImpl(this);
+		return sAccountAuthenticator;
+	}
+
+}
---
+++ src/de/int80/ics/sync/provider/LoginScreen.java
@@ -0,0 +1,11 @@
+package de.int80.ics.sync.provider;
+
+import android.accounts.AccountAuthenticatorActivity;
+
+public class LoginScreen extends AccountAuthenticatorActivity {
+
+	public LoginScreen() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
