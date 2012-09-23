a2aca9ce51564a6cbd026d08edee4c2f9381afb7
Jens Kehne <jens@int80.de>
2012-09-23 12:44:13 +0200

    Add calendar name and sync interval to login screen.

--- src/de/int80/ics/sync/provider/LoginScreen.java
+++ src/de/int80/ics/sync/provider/LoginScreen.java
@@ -10,11 +10,47 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 public class LoginScreen extends AccountAuthenticatorActivity {
 
 	public LoginScreen() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	private void setAutomaticSync(Account account, int intervalIndex, Bundle extras) {
+		int interval = 0;
+		switch(intervalIndex) {
+		case 0:
+			interval = 0;
+			break;
+		case 1:
+			interval = 60;
+			break;
+		case 2:
+			interval = 120;
+			break;
+		case 3:
+			interval = 300;
+			break;
+		case 4:
+			interval = 600;
+			break;
+		case 5:
+			interval = 900;
+			break;
+		case 6:
+			interval = 1800;
+			break;
+		case 7:
+			interval = 2700;
+			break;
+		case 8:
+			interval = 3600;
+			break;
+		}
+		if (interval > 0)
+			ContentResolver.addPeriodicSync(account, "com.android.calendar", extras, interval);
 	}
 
 	@Override
@@ -28,19 +64,22 @@ public class LoginScreen extends AccountAuthenticatorActivity {
 			
 			@Override
 			public void onClick(View v) {
+				EditText calendarName = (EditText) findViewById(R.id.calendarNameField);
 				EditText username = (EditText) findViewById(R.id.userNameField);
 				EditText password = (EditText) findViewById(R.id.passwordField);
 				EditText url = (EditText) findViewById(R.id.calendarURLField);
+				Spinner syncInterval = (Spinner) findViewById(R.id.syncIntervalField);
 
-				Account account = new Account(username.getText().toString(), getString(R.string.ACCOUNT_TYPE));
+				Account account = new Account(calendarName.getText().toString(), getString(R.string.ACCOUNT_TYPE));
 				AccountManager am = AccountManager.get(LoginScreen.this);
 				
 				Bundle extras = new Bundle();
+				extras.putString(getString(R.string.USERNAME_KEY), username.getText().toString());
 				extras.putString(getString(R.string.URL_KEY), url.getText().toString());
 				
 				am.addAccountExplicitly(account, password.getText().toString(), extras);
 
-				ContentResolver.addPeriodicSync(account, "com.android.calendar", extras, 1800);
+				setAutomaticSync(account, syncInterval.getSelectedItemPosition(), extras);
 				
 				Bundle result = new Bundle();
 				result.putString(AccountManager.KEY_ACCOUNT_NAME, username.getText().toString());
