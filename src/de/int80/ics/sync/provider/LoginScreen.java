180865817307406d2e2565d29112ea856ce6f54f
Jens Kehne <jens@int80.de>
2012-09-30 20:16:59 +0200

    Add license

--- src/de/int80/ics/sync/provider/LoginScreen.java
+++ src/de/int80/ics/sync/provider/LoginScreen.java
@@ -1,3 +1,19 @@
+/* Copyright (C) 2012 Jens Kehne
+ *
+ * ICSReader is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free 
+ * Software Foundation; either version 2 of the License, or (at your option) 
+ * any later version.
+ *
+ * ICSReader is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for 
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with ICSReader; if not, see http://www.gnu.org/licenses.
+ */
+
 package de.int80.ics.sync.provider;
 
 import android.accounts.Account;
