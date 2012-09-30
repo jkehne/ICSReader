8af971f5cf1e95df104ca9190bedb56135f370cc
Jens Kehne <jens@int80.de>
2012-09-30 15:55:45 +0200

    Fix calendar creation.

--- src/de/int80/ics/sync/provider/CalendarHandle.java
+++ src/de/int80/ics/sync/provider/CalendarHandle.java
@@ -3,12 +3,20 @@ package de.int80.ics.sync.provider;
 import android.content.ContentValues;
 import android.content.Context;
 import android.net.Uri;
+import android.provider.CalendarContract;
 import android.provider.CalendarContract.Calendars;
 
 public class CalendarHandle {
 	
 	private long calID;
 
+	private static Uri asSyncAdapter(Uri uri, String account, String accountType) {
+	    return uri.buildUpon()
+	        .appendQueryParameter(android.provider.CalendarContract.CALLER_IS_SYNCADAPTER,"true")
+	        .appendQueryParameter(Calendars.ACCOUNT_NAME, account)
+	        .appendQueryParameter(Calendars.ACCOUNT_TYPE, accountType).build();
+	 }
+	
 	public CalendarHandle(Context context, int calID) {
 		this.calID = calID;
 	}
@@ -18,7 +26,10 @@ public class CalendarHandle {
 		values.put(Calendars.ACCOUNT_NAME, name);
 		values.put(Calendars.ACCOUNT_TYPE, type);
 		values.put(Calendars.CALENDAR_DISPLAY_NAME, name);
-		Uri ret = context.getContentResolver().insert(Calendars.CONTENT_URI, values);
+		Uri ret = context.getContentResolver().insert(
+				asSyncAdapter(Calendars.CONTENT_URI, name, type), 
+				values
+				);
 		calID = Long.parseLong(ret.getLastPathSegment());
 	}
 
